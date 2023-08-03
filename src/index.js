31df1436836d18b5245232ae9a63ec296374e0f9
Igor <nhdigor@gmail.com>
2023-08-03 20:05:10 +0300

    19. Redux. Часть 2 / 1. Структура проекта

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { initiateStore } from './store/store'
-import * as actions from './store/actions'
+import configureStore from './store/store'
+import * as actions from './store/tasks/actions'
 
-const store = initiateStore()
+const store = configureStore()
 
 const App = (params) => {
   const [state, setState] = useState(store.getState())
